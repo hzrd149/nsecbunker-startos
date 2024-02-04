c0a23f24ddb402d9ed3a3df3cc127f3a4e8afc2a
hzrd149 <github@hzrd149.com>
2024-02-04 19:54:52 +0000

    update entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,7 +6,10 @@ _term() {
   kill -SIGTERM "$nsecbunkerd_process" 2>/dev/null
 }
 
-node /app/dist/index.js start &
+export DATABASE_URL="file:/root/data/nsecbunker.db"
+
+npx prisma migrate deploy
+node ./dist/index.js start -c /root/data/nsecbunker.json &
 nsecbunkerd_process=$!
 
 trap _term SIGTERM
